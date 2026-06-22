3aedcd90f51d0e29aca47558e7bb388967f7007c
Simon Fang <fangsc249@hotmail.com>
2026-06-22 09:11:58 +0800

    654

--- ConfigTool/ConfigCore/ConfigLoader.cs
+++ ConfigTool/ConfigCore/ConfigLoader.cs
@@ -7,49 +7,49 @@ namespace ConfigTool.ConfigCore
     public static class ConfigLoader // 2025-9-25
     {
         public static string ConfigFolder = AppDomain.CurrentDomain.BaseDirectory;
-        public static T YamlConfig<T>() where T : ConfigBase, new()
+        public static T YamlConfig<T>(string subfolder = "") where T : ConfigBase, new()
         {
-            string configFile = Path.Combine(ConfigFolder, $"{typeof(T).Name}.yaml");
+            string configFile = Path.Combine(ConfigFolder, subfolder, $"{typeof(T).Name}.yaml");
             //Console.WriteLine("Loading config from: {0}", configFile);
             IConfigService configService = new YamlConfigService();
             var config = configService.Load<T>(configFile);
             return config;
         }
-        public static void TabedPanelYamlConfig<T>() where T : ConfigBase, new()
+        public static void TabedPanelYamlConfig<T>(string subfolder = "") where T : ConfigBase, new()
         {
-            string configFile = $@"{ConfigFolder}\{typeof(T).Name}.yaml";
+            string configFile = Path.Combine(ConfigFolder, subfolder, $"{typeof(T).Name}.yaml");
             IConfigService configService = new YamlConfigService();
             var config = configService.Load<T>(configFile);
             new TabedPanelForm(configService, configFile, config).ShowDialog();
 
         }
-        public static void NestedPanelYamlConfig<T>() where T : ConfigBase, new()
+        public static void NestedPanelYamlConfig<T>(string subfolder = "") where T : ConfigBase, new()
         {
-            string configFile = $@"{ConfigFolder}\{typeof(T).Name}.yaml";
+            string configFile = Path.Combine(ConfigFolder, subfolder, $"{typeof(T).Name}.yaml");
             IConfigService configService = new YamlConfigService();
             var config = configService.Load<T>(configFile);
             new NestedConfigPanelForm(configService, config, configFile, typeof(T).Name).ShowDialog();
         }
 
         //***********************************
-        public static T JsonConfig<T>() where T : ConfigBase, new()
+        public static T JsonConfig<T>(string subfolder = "") where T : ConfigBase, new()
         {
-            string configFile = $@"{ConfigFolder}\{typeof(T).Name}.json";
+            string configFile = Path.Combine(ConfigFolder, subfolder, $"{typeof(T).Name}.json");
             IConfigService configService = new JsonConfigService();
             var config = configService.Load<T>(configFile);
             return config;
         }
 
-        public static void TabedPanelJsonConfig<T>() where T : ConfigBase, new()
+        public static void TabedPanelJsonConfig<T>(string subfolder = "") where T : ConfigBase, new()
         {
-            string configFile = $@"{ConfigFolder}\{typeof(T).Name}.json";
+            string configFile = Path.Combine(ConfigFolder, subfolder, $"{typeof(T).Name}.json");
             IConfigService configService = new JsonConfigService();
             var config = configService.Load<T>(configFile);
             new TabedPanelForm(configService, configFile, config).ShowDialog();
         }
-        public static void NestedPanelJsonConfig<T>() where T : ConfigBase, new()
+        public static void NestedPanelJsonConfig<T>(string subfolder = "") where T : ConfigBase, new()
         {
-            string configFile = $@"{ConfigFolder}\{typeof(T).Name}.json";
+            string configFile = Path.Combine(ConfigFolder, subfolder, $"{typeof(T).Name}.json");
             IConfigService configService = new JsonConfigService();
             var config = configService.Load<T>(configFile);
             new NestedConfigPanelForm(configService, config, configFile, typeof(T).Name).ShowDialog();
